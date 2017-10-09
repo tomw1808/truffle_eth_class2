be82193569ef2390fefc8357296625a38a39b569
Thomas Wiesner <thomas@moschitz.at>
2017-10-09 04:25:55 +0200

    Added simpleWallet to the Repository

---
+++ s09 - simpleWallet/SimpleWallet - ASSIGNMENT.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.17;
+
+import "github.com/ethereum/solidity/std/mortal.sol";
+
+contract SimpleWallet is mortal {
+    
+    mapping(address => Permission) myAddressMapping;
+    
+    struct Permission {
+        bool isAllowed;
+        uint maxTransferAmount;
+    }
+    
+    function addAddressToSendersList(address permitted, uint maxTransferAmount) public onlyowner {
+        myAddressMapping[permitted] = Permission(true, maxTransferAmount);
+    }
+    
+    function sendFunds(address receiver, uint amountInWei) public {
+        require(myAddressMapping[msg.sender].isAllowed);
+        require(myAddressMapping[msg.sender].maxTransferAmount <= amountInWei);
+        receiver.transfer(amountInWei);
+    }
+
+    
+    function () public payable {}
+    
+}
---
+++ s09 - simpleWallet/SimpleWallet - SOLUTION.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.17;
+
+import "github.com/ethereum/solidity/std/mortal.sol";
+
+contract SimpleWallet is mortal {
+
+    mapping(address => Permission) myAddressMapping;
+
+    struct Permission {
+    bool isAllowed;
+    uint maxTransferAmount;
+    }
+
+    function SimpleWallet() public {
+        //Automatically add the creator of the wallet to the permitted senders list. Makes things easier.
+        myAddressMapping[msg.sender] = Permission(true, 5000 ether);
+    }
+
+    function addAddressToSendersList(address permitted, uint maxTransferAmount) public onlyowner {
+        myAddressMapping[permitted] = Permission(true, maxTransferAmount);
+    }
+
+    function removeAddressFromSendersList(address theAddress) public onlyowner {
+        //a simple delete solves our problem
+        delete myAddressMapping[theAddress];
+        /**
+         * Different solution:
+         * myAddressMapping[theAddress].isAllowed = false;
+         * */
+    }
+
+    function sendFunds(address receiver, uint amountInWei) public {
+        require(myAddressMapping[msg.sender].isAllowed);
+        require(myAddressMapping[msg.sender].maxTransferAmount >= amountInWei); //the amount in "the bank" must be larger than the amount taken out.
+        receiver.transfer(amountInWei);
+    }
+
+
+    function () public payable {}
+
+}
